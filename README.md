8ed3f4adff2ca77812bdf077b3eecf8c0e4bdd6a
Douglas <japanmarsalis@gmail.com>
2023-08-03 13:56:24 -0500

    After tutor Thursday 2 gitignore, added code

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ WHEN I click on the Write icon in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 
 ## Sources
+* Express.js inormation- https://expressjs.com/en/starter/basic-routing.html
 * RICE UNIVERSITY Boot Camp Activities for Express.js
 * Special thanks to my tutor Ben.
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,42 +1,34 @@
 const fs = require("fs");
 //var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+const { readFromFile, readAndAppend } = require('../utils/fsUtils');
+const uuid = require("../utils/uuid.js");
 const router = require('express').Router();
 
 //Get Route for a New Note
 router.get('/', (req, res) => {
-    console.log("Hello");
-    fs.readFile("./db/db.json", (err, data) => {
-        console.log(data);
-        if (err) {
-            console.log(err);
-        }
-        res.json(data)
+    readFromFile("./db/db.json")
+    .then((data) =>  {
+        res.json(JSON.parse(data));
     });
  });
-
-// GET Route for retrieving all the notes
-// router.get('/', (req, res) => {
-//     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-//     console.log(data);
-// });
 
  //Post Route for a new Note
 router.post('/', (req, res) => {
     console.log(req.body);
 
-    const { noteTitle, note, noteId} = req.body;
+    const { title, text } = req.body;
 
     if (req.body) {
         const newNote = {
-            noteTitle,
-            note,
-            noteId: uuid(),
+            title,
+            text,
+            id: uuid(),
         };
 
         readAndAppend(newNote, './db/db.json');
         res.json(`Note added successfully!`);
     } else {
-        res.error('Error in adding note!');
+        res.json('Error in adding note!');
     }
 });
 
